ff43ee3dd1f1c101d5dab7e171e30e2afff321da
Pierre Janineh <janinehpierre@gmail.com>
2023-11-16 09:53:27 +0200

    fixed supported versions for different Platforms

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ClukUI",
     platforms: [
-        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
