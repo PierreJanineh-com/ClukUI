e0296530665d763a1d3ef2a46ae60d93add85e85
Pierre Janineh <janinehpierre@gmail.com>
2023-11-17 16:18:10 +0200

    Created Configuration class and builder class

---
+++ Sources/ClukUI/Cluk/Configurations/ClukConfigurationsBuilder.swift
@@ -0,0 +1,14 @@
+//
+//  ClukConfigurationsBuilder.swift
+//
+//
+//  Created by Pierre Janineh on 17/11/2023.
+//
+
+import Foundation
+
+internal class ClukConfigurationsBuilder {
+    var timeTextIsHidden: Bool = false
+    
+    init() {}
+}
